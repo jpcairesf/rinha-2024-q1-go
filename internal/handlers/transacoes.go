d3517541fc70bff686ffac6b4901707d4ead7b77
jpcairesf <jpcairesf@gmail.com>
2024-02-23 15:57:33 -0300

    feat: Adding validations to TransacaoRequest

--- internal/handlers/transacoes.go
+++ internal/handlers/transacoes.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"unicode/utf8"
 
 	"github.com/jpcairesf/rinha-2024-q1-go/internal/db"
 )
@@ -24,10 +25,17 @@ type TransacaoResponse struct {
 	Limite int64 `json:"limite"`
 }
 
+func (t *TransacaoRequest) isNotValid() bool {
+	tipoValido := t.Tipo != "c" && t.Tipo != "d"
+	descricaoValida := utf8.RuneCountInString(t.Descricao) == 0 || utf8.RuneCountInString(t.Descricao) > 10
+	valorValido := t.Valor >= 0
+	return !(tipoValido && descricaoValida && valorValido)
+}
+
 func PostTransacao(w http.ResponseWriter, r *http.Request) {
 	var request TransacaoRequest
 	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
+	if err != nil || request.isNotValid() {
 		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
